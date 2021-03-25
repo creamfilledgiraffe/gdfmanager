06259cca96bd3bc620d73c70cddb809b652738ac
Richard Wayne <creamfilledgiraffe@gmail.com>
2021-03-24 23:42:02 -0400

    Added copy of the BSD-3-clause license

---
+++ gdfm.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#include "compiler.h"
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
+
